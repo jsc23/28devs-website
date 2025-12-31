05c5fff51be69a342d0b3d5bc4f6db4c5e456572
Javier <j.sanchezcad23@gmail.com>
2025-12-31 17:56:22 +0100

    updated menu on the web

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -2,18 +2,6 @@ import { Menu } from "@/types/menu";
 
 const menuData: Menu[] = [
   {
-    id: 1,
-    title: "Home",
-    path: "/",
-    newTab: false,
-  },
-  {
-    id: 2,
-    title: "About",
-    path: "/about",
-    newTab: false,
-  },
-  {
     id: 33,
     title: "Blog",
     path: "/blog",
@@ -21,13 +9,13 @@ const menuData: Menu[] = [
   },
   {
     id: 3,
-    title: "Support",
+    title: "Help Center",
     path: "/contact",
     newTab: false,
   },
   {
     id: 4,
-    title: "Pages",
+    title: "Solutions",
     newTab: false,
     submenu: [
       {
@@ -79,6 +67,68 @@ const menuData: Menu[] = [
         newTab: false,
       },
     ],
+  },
+  ,
+  {
+    id: 4,
+    title: "Apps for Zendesk",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "About Page",
+        path: "/about",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Contact Page",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "Blog Grid Page",
+        path: "/blog",
+        newTab: false,
+      },
+      {
+        id: 44,
+        title: "Blog Sidebar Page",
+        path: "/blog-sidebar",
+        newTab: false,
+      },
+      {
+        id: 45,
+        title: "Blog Details Page",
+        path: "/blog-details",
+        newTab: false,
+      },
+      {
+        id: 46,
+        title: "Sign In Page",
+        path: "/signin",
+        newTab: false,
+      },
+      {
+        id: 47,
+        title: "Sign Up Page",
+        path: "/signup",
+        newTab: false,
+      },
+      {
+        id: 48,
+        title: "Error Page",
+        path: "/error",
+        newTab: false,
+      },
+    ],
+  },
+  {
+    id: 33,
+    title: "Pricing",
+    path: "/blog",
+    newTab: false,
   },
 ];
 export default menuData;
